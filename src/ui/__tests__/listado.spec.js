a43f025f5ee63d18698efcf5c9aba1490073e3d3
Lucas <lucasdgonzalez@outlook.com>
2024-04-05 22:00:25 -0300

    Testea la función 'mostrarListadoPokemones'

--- src/ui/__tests__/listado.spec.js
+++ src/ui/__tests__/listado.spec.js
@@ -1,4 +1,5 @@
 import { actualizarTextoIndicePokemones, mostrarListadoPokemones } from '../listado.js';
+import fixtureListadoPagina1 from "../../../cypress/fixtures/listado-pagina-1.json";
 
 describe("actualizarTextoIndicePokemones", () => {
   it("Debería actualizar el texto del índice", () => {
@@ -8,5 +9,26 @@ describe("actualizarTextoIndicePokemones", () => {
     const $indice = document.querySelector('#indice');
 
     expect($indice.textContent).toBe('Se viene Booooca');
+  });
+});
+
+describe("mostrarListadoPokemones", () => {
+  it("Muestra el listado de los pokémon", () => {
+    document.body.innerHTML = '<div id="indice"></div>';
+
+    const listadoNombresPokemones = [];
+
+    fixtureListadoPagina1.results.forEach(pokemon => {
+      const nombrePokemon = pokemon["name"];
+      listadoNombresPokemones.push(nombrePokemon);
+    });
+
+    mostrarListadoPokemones(listadoNombresPokemones);
+
+    const $indice = document.querySelector('#indice');
+
+    listadoNombresPokemones.forEach(nombre => {
+      expect($indice.textContent).toContain(nombre);
+    });
   });
 });
