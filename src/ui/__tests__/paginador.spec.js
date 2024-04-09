120c1f3703644b51376b811f6bee028f87dc08f0
Lucas <lucasdgonzalez@outlook.com>
2024-04-08 21:01:05 -0300

    Prueba ejecutar función asignada a botón del paginador

--- src/ui/__tests__/paginador.spec.js
+++ src/ui/__tests__/paginador.spec.js
@@ -2,9 +2,11 @@ import { manejarCambioPagina } from '../paginador.js';
 import mostrarPaginador from '../paginador.js';
 
 describe("mostrarPaginador", () => { 
-  it("Muestra el paginador", () => {
+  beforeEach(() => {
     document.body.innerHTML = '<div id="paginador"></div>';
+  });
 
+  it("Muestra el paginador", () => {
     const $paginador = document.querySelector("#paginador");
     expect($paginador.children.length).toBe(0);
 
@@ -15,5 +17,19 @@ describe("mostrarPaginador", () => {
 
     mostrarPaginador(totalPokemones, paginaActual, urlSiguiente, urlAnterior, () => {});
     expect($paginador.children.length).toBeGreaterThan(0);
+  });
+
+  it("Ejecuta la función asignada luego de dar click al paginador", async () => {
+    const funcionAEspiar = jest.fn();
+    
+    const totalPokemones = 1302;
+    const paginaActual = 1;
+    const urlSiguiente = null;
+    const urlAnterior = true;
+
+    mostrarPaginador(totalPokemones, paginaActual, urlSiguiente, urlAnterior, funcionAEspiar);
+    const eventoClick = new Event('click');
+    document.querySelector('#paginador').dispatchEvent(eventoClick);
+    expect(funcionAEspiar).toHaveBeenCalled();
   });
 });
